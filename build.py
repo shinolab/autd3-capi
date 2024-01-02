7b159c769c63613f76b76b13e6ac799349fd5f2d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-02 17:37:48 +0900

    update build.py

--- build.py
+++ build.py
@@ -22,6 +22,7 @@ import re
 import shutil
 import subprocess
 import sys
+from shutil import which
 from typing import Optional
 
 
@@ -47,11 +48,16 @@ def working_dir(path):
         os.chdir(cwd)
 
 
+def env_exists(value):
+    return value in os.environ and os.environ[value] != ""
+
+
 class Config:
     _platform: str
     _all: bool
     release: bool
     shaderc: bool
+    cuda: bool
     target: Optional[str]
     universal: bool
     no_examples: bool
@@ -69,7 +75,7 @@ class Config:
         self.universal = hasattr(args, "universal") and args.universal
         self.no_examples = hasattr(args, "no_examples") and args.no_examples
 
-        self.cuda = False if self.is_macos() else True
+        self.cuda = False if self.is_macos() else self.is_cuda_available()
 
         if self.is_linux() and hasattr(args, "arch") and args.arch is not None:
             self.shaderc = False
@@ -83,8 +89,29 @@ class Config:
                     err(f'arch "{args.arch}" is not supported.')
                     sys.exit(-1)
         else:
-            self.shaderc = True
             self.target = None
+            if self.is_shaderc_available():
+                self.shaderc = True
+            else:
+                self.shaderc = False
+
+    def is_shaderc_available(self):
+        shaderc_lib_name = (
+            "shaderc_combined.lib" if self.is_windows() else "libshaderc_combined.a"
+        )
+        if env_exists("SHADERC_LIB_DIR"):
+            if os.path.isfile(f"{os.environ['SHADERC_LIB_DIR']}/{shaderc_lib_name}"):
+                return True
+        if env_exists("VULKAN_SDK"):
+            if os.path.isfile(f"{os.environ['VULKAN_SDK']}/lib/{shaderc_lib_name}"):
+                return True
+        if not self.is_windows():
+            if os.path.isfile(f"/usr/local/lib/{shaderc_lib_name}"):
+                return True
+        return False
+
+    def is_cuda_available(self):
+        return which("nvcc") is not None
 
     def cargo_command_base(self, subcommand):
         command = []
