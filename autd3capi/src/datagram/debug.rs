c1703f957f97c911ca9babcbb4d9858aa0750e85
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-08 15:56:51 +0900

    add AUTDDatagramConfigureDebugSettings2

--- autd3capi/src/datagram/debug.rs
+++ autd3capi/src/datagram/debug.rs
@@ -21,3 +21,37 @@ pub unsafe extern "C" fn AUTDDatagramConfigureDebugSettings(
     )
     .into()
 }
+
+#[no_mangle]
+#[must_use]
+pub unsafe extern "C" fn AUTDDatagramConfigureDebugSettings2(
+    types: *const u8,
+    values: *const u16,
+    geometry: GeometryPtr,
+) -> DatagramPtr {
+    DynamicConfigureDebugSettings::new(
+        geometry
+            .devices()
+            .map(|dev| {
+                (
+                    dev.idx(),
+                    DebugSettings {
+                        ty: [
+                            types.add(4 * dev.idx()).read(),
+                            types.add(4 * dev.idx() + 1).read(),
+                            types.add(4 * dev.idx() + 2).read(),
+                            types.add(4 * dev.idx() + 3).read(),
+                        ],
+                        value: [
+                            values.add(4 * dev.idx()).read(),
+                            values.add(4 * dev.idx() + 1).read(),
+                            values.add(4 * dev.idx() + 2).read(),
+                            values.add(4 * dev.idx() + 3).read(),
+                        ],
+                    },
+                )
+            })
+            .collect(),
+    )
+    .into()
+}
