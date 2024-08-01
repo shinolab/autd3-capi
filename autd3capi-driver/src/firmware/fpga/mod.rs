e4613d40e87bc06cebcaf2fe442cc39176c602e8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-01 09:40:56 +0900

    rm unused codes

--- autd3capi-driver/src/firmware/fpga/mod.rs
+++ autd3capi-driver/src/firmware/fpga/mod.rs
@@ -4,7 +4,6 @@ mod gpio;
 mod loop_behavior;
 mod segment;
 mod silencer_target;
-mod stm_sampling_config;
 mod transition_mode;
 
 pub use debug_type::*;
@@ -13,5 +12,4 @@ pub use gpio::*;
 pub use loop_behavior::*;
 pub use segment::*;
 pub use silencer_target::*;
-pub use stm_sampling_config::*;
 pub use transition_mode::*;
