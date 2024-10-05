6c0de74db2528a34989b22eddf5c28c7fb5e30fd
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-06 08:49:41 +0900

    bump to v28.0.0-rc.0

--- autd3capi/src/modulation/fourier.rs
+++ autd3capi/src/modulation/fourier.rs
@@ -13,10 +13,20 @@ use autd3capi_driver::{
 pub unsafe extern "C" fn AUTDModulationFourierExact(
     components: *const ModulationPtr,
     size: u32,
+    clamp: bool,
+    scale_factor: f32,
     loop_behavior: LoopBehavior,
 ) -> ResultModulation {
     Fourier::new((0..size as usize).map(|i| **take_mod!(components.add(i).read(), Sine<ExactFreq>)))
-        .map(|f| f.with_loop_behavior(loop_behavior.into()))
+        .map(|f| {
+            f.with_clamp(clamp)
+                .with_scale_factor(if scale_factor.is_nan() {
+                    None
+                } else {
+                    Some(scale_factor)
+                })
+                .with_loop_behavior(loop_behavior.into())
+        })
         .into()
 }
 
@@ -25,12 +35,22 @@ pub unsafe extern "C" fn AUTDModulationFourierExact(
 pub unsafe extern "C" fn AUTDModulationFourierExactFloat(
     components: *const ModulationPtr,
     size: u32,
+    clamp: bool,
+    scale_factor: f32,
     loop_behavior: LoopBehavior,
 ) -> ResultModulation {
     Fourier::new(
         (0..size as usize).map(|i| **take_mod!(components.add(i).read(), Sine<ExactFreqFloat>)),
     )
-    .map(|f| f.with_loop_behavior(loop_behavior.into()))
+    .map(|f| {
+        f.with_clamp(clamp)
+            .with_scale_factor(if scale_factor.is_nan() {
+                None
+            } else {
+                Some(scale_factor)
+            })
+            .with_loop_behavior(loop_behavior.into())
+    })
     .into()
 }
 
@@ -39,11 +59,21 @@ pub unsafe extern "C" fn AUTDModulationFourierExactFloat(
 pub unsafe extern "C" fn AUTDModulationFourierNearest(
     components: *const ModulationPtr,
     size: u32,
+    clamp: bool,
+    scale_factor: f32,
     loop_behavior: LoopBehavior,
 ) -> ResultModulation {
     Fourier::new(
         (0..size as usize).map(|i| **take_mod!(components.add(i).read(), Sine<NearestFreq>)),
     )
-    .map(|f| f.with_loop_behavior(loop_behavior.into()))
+    .map(|f| {
+        f.with_clamp(clamp)
+            .with_scale_factor(if scale_factor.is_nan() {
+                None
+            } else {
+                Some(scale_factor)
+            })
+            .with_loop_behavior(loop_behavior.into())
+    })
     .into()
 }
