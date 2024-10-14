93352fe5f8d3ce60f73f82cc2719cae4d0cc9f01
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-14 10:13:24 +0900

    bump to v28.0.1

--- autd3capi/src/gain/mod.rs
+++ autd3capi/src/gain/mod.rs
@@ -1,9 +1,7 @@
-use autd3::derive::GainCalcFn;
-use autd3capi_driver::{
-    autd3::prelude::IntoDatagramWithSegment, driver::error::AUTDInternalError, *,
-};
+use autd3capi_driver::{autd3::prelude::IntoDatagramWithSegment, *};
 
 pub mod bessel;
+pub mod cache;
 pub mod custom;
 pub mod focus;
 pub mod group;
@@ -12,50 +10,6 @@ pub mod plane;
 pub mod raw;
 pub mod transform;
 pub mod uniform;
-
-#[repr(C)]
-pub struct GainCalcPtr(pub *const libc::c_void);
-
-#[repr(C)]
-
-pub struct ResultGainCalcDrivesMap {
-    pub result: GainCalcPtr,
-    pub err_len: u32,
-    pub err: ConstPtr,
-}
-
-impl<'a> From<Result<GainCalcFn<'a>, AUTDInternalError>> for ResultGainCalcDrivesMap {
-    fn from(r: Result<GainCalcFn<'a>, AUTDInternalError>) -> Self {
-        match r {
-            Ok(v) => Self {
-                result: GainCalcPtr(Box::into_raw(Box::new(v)) as _),
-                err_len: 0,
-                err: ConstPtr(std::ptr::null_mut()),
-            },
-            Err(e) => {
-                let err = e.to_string();
-                Self {
-                    result: GainCalcPtr(std::ptr::null()),
-                    err_len: err.as_bytes().len() as u32 + 1,
-                    err: ConstPtr(Box::into_raw(Box::new(err)) as _),
-                }
-            }
-        }
-    }
-}
-
-impl std::ops::Deref for GainCalcPtr {
-    type Target = GainCalcFn<'static>;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { (self.0 as *const Self::Target).as_ref().unwrap() }
-    }
-}
-impl std::ops::DerefMut for GainCalcPtr {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { (self.0 as *mut Self::Target).as_mut().unwrap() }
-    }
-}
 
 #[no_mangle]
 #[must_use]
@@ -73,41 +27,4 @@ pub unsafe extern "C" fn AUTDGainIntoDatagramWithSegment(
 #[must_use]
 pub unsafe extern "C" fn AUTDGainIntoDatagram(gain: GainPtr) -> DatagramPtr {
     (*take!(gain, Box<G>)).into()
-}
-
-#[no_mangle]
-#[must_use]
-pub unsafe extern "C" fn AUTDGainCalc(
-    gain: GainPtr,
-    geometry: GeometryPtr,
-) -> ResultGainCalcDrivesMap {
-    (gain.0 as *mut Box<G>)
-        .as_ref()
-        .unwrap()
-        .calc(&geometry)
-        .into()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn AUTDGainFree(gain: GainPtr) {
-    let _ = take!(gain, Box<G>);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn AUTDGainCalcGetResult(
-    src: GainCalcPtr,
-    dst: *mut Drive,
-    device: DevicePtr,
-) {
-    let mut src = src;
-    let src = src(&device);
-    let dst = dst as *mut autd3::prelude::Drive;
-    device
-        .iter()
-        .for_each(|tr| dst.add(tr.idx()).write(src(tr)));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn AUTDGainCalcFreeResult(src: GainCalcPtr) {
-    let _ = take!(src, GainCalcFn<'static>);
 }
