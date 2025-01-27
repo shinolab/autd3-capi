c30b20cc22c44cdb3b4846afa59299201117097d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-27 16:17:50 +0900

    update `AUTDGainBessel`

--- autd3capi/src/gain/bessel.rs
+++ autd3capi/src/gain/bessel.rs
@@ -1,22 +1,24 @@
 use autd3capi_driver::{
     autd3::gain::{Bessel, BesselOption},
-    driver::geometry::Vector3,
+    driver::{
+        defined::Angle,
+        geometry::{UnitVector3, Vector3},
+    },
     *,
 };
-use driver::{defined::rad, geometry::UnitVector3};
 
 #[no_mangle]
 #[must_use]
 pub unsafe extern "C" fn AUTDGainBessel(
     pos: Point3,
     dir: Vector3,
-    theta: f32,
+    theta: Angle,
     option: BesselOption,
 ) -> GainPtr {
     Bessel {
         pos,
         dir: UnitVector3::new_normalize(dir),
-        theta: theta * rad,
+        theta,
         option,
     }
     .into()
